734c7ee50da9ead2e4d2374ca90dadf2d5c34761
hanaa1029 <hana.a.ahmed.09206@gmail.com>
2025-11-29 15:06:31 +0300

    v1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        Arrayss arr= new Arrayss();
+        int []numbers={1,2,3,4,5};
+        // arr.update(numbers,9,4);
+        //arr.delete(numbers,3);
+        arr.shiftLeft_delete(numbers,2);
+        arr.Traversal(numbers)
+
+
+
+
+
+        ///int []numbers=new int[5];
+        //  arr.input(numbers);
+        // arr.ReverseTraversal(numbers);
+        ;
+
+
+
+
+
+
+
+        //System.out.println(Arrays.toString(numbers));
+
+
+
+
+
+    }
+}
